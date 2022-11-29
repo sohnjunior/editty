b6a53208557a40eba0bda8b4dea6cd29ca1007b3
sohnjunior <sohnjunior@naver.com>
2022-11-29 21:45:18 +0900

    🔧 storybook path alias 설정

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,14 +1,15 @@
+const path = require('path')
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "framework": "@storybook/web-components",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: '@storybook/web-components',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: async (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, '../src/')
+    config.resolve.alias['@atoms'] = path.resolve(__dirname, '../src/components/atoms')
+    return config
+  },
 }
