01444fbcccacc0f3ff9dceca00da8c7b8179a992
sohnjunior <sohnjunior@naver.com>
2022-12-02 00:33:07 +0900

    ♻️ container 구조 변경

--- client/src/components/atoms/container.ts
+++ client/src/components/atoms/container.ts
@@ -1,36 +1,26 @@
+const template = document.createElement('template')
+template.innerHTML = `
+  <style>
+    :host {
+      all: unset;
+      width: fit-content;
+      padding: 15px 20px;
+      background: #fafafa;
+      border-radius: 20px;
+      filter: drop-shadow(0px 9px 17px rgba(125, 159, 192, 0.12));
+    }
+  </style>
+  <slot></slot>
+`
+
 export default class Container extends HTMLElement {
   private $root: ShadowRoot
 
-  static displayName = 'atom-container'
+  static tag = 'v-container'
 
   constructor() {
     super()
     this.$root = this.attachShadow({ mode: 'open' })
-  }
-
-  connectedCallback() {
-    this.render()
-  }
-
-  template() {
-    return `
-      <style>
-        :host > div {
-          all: unset;
-          width: fit-content;
-          padding: 15px 20px;
-          background: #fafafa;
-          border-radius: 20px;
-          filter: drop-shadow(0px 9px 17px rgba(125, 159, 192, 0.12));
-        }
-      </style>
-      <div>
-        <slot></slot>
-      </div>
-    `
-  }
-
-  render() {
-    this.$root.innerHTML = this.template()
+    this.$root.appendChild(template.content.cloneNode(true))
   }
 }
--- client/src/stories/container.stories.ts
+++ client/src/stories/container.stories.ts
@@ -2,10 +2,10 @@ import { html } from 'lit-html'
 
 import Container from '@atoms/container'
 
-customElements.define(Container.displayName, Container)
+customElements.define(Container.tag, Container)
 
 export default {
   title: 'Atoms / Container',
 }
 
-export const Basic = () => html`<atom-container>자식</atom-container>`
+export const Basic = () => html`<v-container>자식</v-container>`
