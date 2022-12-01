fe451341b376b311a8be5c3a65e89363d72cbe65
sohnjunior <sohnjunior@naver.com>
2022-12-02 01:34:47 +0900

    ♻️ Button 컴포넌트의 template 코드를 분리

--- client/src/components/atoms/button.ts
+++ client/src/components/atoms/button.ts
@@ -1,40 +1,38 @@
-export default class Button extends HTMLElement {
+const template = document.createElement('template')
+template.innerHTML = `
+  <style>
+  :host > button {
+    color: blue;
+  }
+  </style>
+  <button>버튼</button>
+`
+
+export default class VButton extends HTMLElement {
   private $root: ShadowRoot
 
-  static displayName = 'atom-button'
+  static tag = 'v-button'
   static get observedAttributes() {
     return ['color']
   }
 
   get color() {
+    /** TODO: getter - setter @Prop 으로 변경 */
     return this.getAttribute('color')
   }
 
   constructor() {
     super()
     this.$root = this.attachShadow({ mode: 'open' })
+    this.$root.appendChild(template.content.cloneNode(true))
   }
 
-  connectedCallback() {
-    this.render()
-  }
-
-  attributeChangedCallback() {
-    this.render()
-  }
-
-  template() {
-    return `
-      <style>
-        :host > button {
-          color: ${this.color}
-        }
-      </style>
-      <button>버튼</button>
-    `
-  }
-
-  render() {
-    this.$root.innerHTML = this.template()
+  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+    switch (name) {
+      case 'color': {
+        const $button = this.$root.querySelector('button')
+        $button && ($button.style.color = newValue)
+      }
+    }
   }
 }
--- client/src/stories/button.stories.ts
+++ client/src/stories/button.stories.ts
@@ -2,14 +2,14 @@ import { html } from 'lit-html'
 
 import Button from '@atoms/button'
 
-customElements.define(Button.displayName, Button)
+customElements.define(Button.tag, Button)
 
 export default {
   title: 'Atoms / Button',
 }
 
 export const Basic = ({ color, onClick }: { color: string; onClick: () => void }) =>
-  html`<atom-button color="${color}" @click="${onClick}"></atom-button>`
+  html`<v-button color="${color}" @click="${onClick}"></v-button>`
 Basic.args = {
   color: 'blue',
   onClick: () => console.log('clicked!'),
