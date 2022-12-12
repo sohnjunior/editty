7b5dd99a62868e9a618bb4c6850065016fc979b5
owen-triple <37819666+sohnjunior@users.noreply.github.com>
2022-12-12 21:53:39 +0900

    🧪 icon button story 추가

---
+++ client/src/stories/icon-button.stories.ts
@@ -0,0 +1,26 @@
+import { html } from 'lit-html'
+
+import IconButton from '@atoms/icon-button'
+import type { Icon, Size } from '@atoms/icon-button'
+
+customElements.define(IconButton.tag, IconButton)
+
+export default {
+  title: 'Atoms / Icon Button',
+  argTypes: {
+    icon: { control: 'select', options: ['delete', 'cursor', 'pen'] },
+    size: { control: 'select', options: ['small', 'medium', 'large'] },
+  },
+}
+
+interface BasicProps {
+  icon: Icon
+  size: Size
+}
+
+export const Basic = ({ icon, size }: BasicProps) =>
+  html`<v-icon-button icon="${icon}" size="${size}"></v-icon-button>`
+Basic.args = {
+  icon: 'delete',
+  size: 'medium',
+}
