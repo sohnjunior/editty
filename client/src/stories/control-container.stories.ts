8f89a199f7ec601df9ee71804bf283d910387a6f
owen-triple <37819666+sohnjunior@users.noreply.github.com>
2022-12-23 17:43:50 +0900

    🧪 control container story 추가

---
+++ client/src/stories/control-container.stories.ts
@@ -0,0 +1,11 @@
+import { html } from 'lit-html'
+
+import ControlContainer from '@/components/molecules/control-container'
+
+customElements.define(ControlContainer.tag, ControlContainer)
+
+export default {
+  title: 'Molecules / Control Container',
+}
+
+export const Basic = () => html`<v-control-container></v-control-container>`
