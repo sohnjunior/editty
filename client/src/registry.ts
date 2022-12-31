1564d1c376e78adddce73d9d9674670c4cbd2cbc
owen-triple <37819666+sohnjunior@users.noreply.github.com>
2022-12-31 17:11:58 +0900

    ✨ custom component 등록

--- client/src/registry.ts
+++ client/src/registry.ts
@@ -1,9 +1,17 @@
 import MobileLayout from '@layouts/mobile'
-import Button from '@/components/elements/button'
-import Canvas from '@/components/elements/canvas'
+import Button from '@elements/button'
+import Canvas from '@elements/canvas'
+import IconButton from '@elements/icon-button'
+import Container from '@elements/container'
+import ControlContainer from '@molecules/control-container'
+import App from './app'
 
 export function defineCustomElements() {
+  customElements.define(App.tag, App)
   customElements.define(MobileLayout.tag, MobileLayout)
   customElements.define(Button.tag, Button)
+  customElements.define(IconButton.tag, IconButton)
   customElements.define(Canvas.tag, Canvas)
+  customElements.define(Container.tag, Container)
+  customElements.define(ControlContainer.tag, ControlContainer)
 }
