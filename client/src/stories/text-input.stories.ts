06f5723934a2c831d6e2995df346009356633dce
owen-triple <37819666+sohnjunior@users.noreply.github.com>
2023-03-01 17:27:04 +0900

    🧪 text input story 추가

--- client/src/stories/text-input.stories.ts
+++ client/src/stories/text-input.stories.ts
@@ -8,4 +8,14 @@ export default {
   title: 'Elements / Text Input',
 }
 
-export const Basic = () => html`<v-text-input></v-text-input>`
+interface BasicProps {
+  placeholder: string
+  onChange: (e: CustomEvent) => void
+}
+
+export const Basic = ({ placeholder, onChange }: BasicProps) =>
+  html`<v-text-input @change="${onChange}" placeholder="${placeholder}"></v-text-input>`
+Basic.args = {
+  placeholder: 'input example',
+  onChange: (e: CustomEvent) => console.log(e.detail.value),
+}
