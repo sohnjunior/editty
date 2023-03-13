72ddc6c0a60cca5d4e83bfa547746f664197584e
owen-triple <37819666+sohnjunior@users.noreply.github.com>
2023-03-13 19:19:27 +0900

    ✨ Register Icon and TextInput elements

--- client/src/registry.ts
+++ client/src/registry.ts
@@ -1,8 +1,10 @@
 import MobileLayout from '@layouts/mobile'
 import Button from '@elements/button'
 import Canvas from '@elements/canvas'
+import Icon from '@elements/icon'
 import IconButton from '@elements/icon-button'
 import Container from '@elements/container'
+import TextInput from '@elements/text-input'
 import ControlContainer from '@molecules/control-container'
 import HistoryContainer from '@molecules/history-container'
 import App from './app'
@@ -11,9 +13,11 @@ export function defineCustomElements() {
   customElements.define(App.tag, App)
   customElements.define(MobileLayout.tag, MobileLayout)
   customElements.define(Button.tag, Button)
+  customElements.define(Icon.tag, Icon)
   customElements.define(IconButton.tag, IconButton)
   customElements.define(Canvas.tag, Canvas)
   customElements.define(Container.tag, Container)
+  customElements.define(TextInput.tag, TextInput)
   customElements.define(ControlContainer.tag, ControlContainer)
   customElements.define(HistoryContainer.tag, HistoryContainer)
 }
