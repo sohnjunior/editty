7169794df99b32bae8ca78d490a800d909094c8f
owen-triple <37819666+sohnjunior@users.noreply.github.com>
2023-03-23 21:45:10 +0900

    ✅ Fix jest config

--- client/jest.config.js
+++ client/jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
 }
