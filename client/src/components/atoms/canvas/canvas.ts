6867539c598eab44a83064df19fd7bea7ae163b7
owen-triple <37819666+sohnjunior@users.noreply.github.com>
2023-03-26 22:14:39 +0900

    ✨ Add basic canvas pencil mode (#9)

--- client/src/components/atoms/canvas/canvas.ts
+++ client/src/components/atoms/canvas/canvas.ts
@@ -3,23 +3,33 @@ template.innerHTML = `
   <style>
     :host {
       display: block;
-      padding: 0;
       width: 100%;
       height: 100%;
+      margin: 0;
+      padding: 0;
     }
 
-    #canvas {
+    :host > #canvas-container {
       width: 100%;
       height: 100%;
     }
   </style>
-  <canvas id="canvas"></canvas>
+  <canvas id="canvas-container"></canvas>
 `
+
+/** TODO: 전역상태로 변경하기 */
+const snapshots: ImageData[] = []
+
+interface PencilPoint {
+  x: number
+  y: number
+}
 
 export default class VCanvas extends HTMLElement {
   private $root!: ShadowRoot
   private $canvas!: HTMLCanvasElement
   private context!: CanvasRenderingContext2D
+  private points: PencilPoint[] = []
 
   static tag = 'v-canvas'
 
@@ -30,35 +40,171 @@ export default class VCanvas extends HTMLElement {
     }
 
     const initCanvas = () => {
-      this.$canvas = this.$root.getElementById('canvas') as HTMLCanvasElement
+      this.$canvas = this.$root.getElementById('canvas-container') as HTMLCanvasElement
       const ctx = this.$canvas.getContext('2d')
       if (!ctx) {
         throw new Error('🚨 canvas load fail')
       }
       this.context = ctx
-    }
 
-    const initCanvasStyle = () => {
       this.context.fillStyle = '#f8f8f8'
       this.context.fillRect(0, 0, this.$canvas.width, this.$canvas.height)
-    }
-
-    const refineCanvasRatio = () => {
-      const ratio = window.devicePixelRatio
-      const { width, height } = getComputedStyle(this.$canvas)
-      this.$canvas.width = parseInt(width) * ratio
-      this.$canvas.height = parseInt(height) * ratio
     }
 
     super()
     initShadowRoot()
     initCanvas()
-    refineCanvasRatio()
-    initCanvasStyle()
   }
 
   connectedCallback() {
-    this.context.fillStyle = 'rgba(0, 0, 200, 0.5)'
-    this.context.fillRect(30, 30, 50, 50)
+    const initEvents = () => {
+      this.addEventListener('mousedown', this.setup)
+      this.addEventListener('mouseup', this.cleanup)
+      this.addEventListener('mouseleave', this.cleanup)
+
+      this.addEventListener('touchstart', this.setup)
+      this.addEventListener('touchend', this.cleanup)
+    }
+
+    const refineCanvasRatio = () => {
+      const ratio = window.devicePixelRatio
+      const { width, height } = getComputedStyle(this.$canvas)
+
+      this.$canvas.width = parseInt(width) * ratio
+      this.$canvas.height = parseInt(height) * ratio
+
+      fillBackgroundColor(this.$canvas, '#f8f8f8')
+    }
+
+    refineCanvasRatio()
+    initEvents()
   }
+
+  disconnectedCallback() {
+    this.removeEventListener('mousemove', this.draw)
+    this.removeEventListener('touchmove', this.draw)
+  }
+
+  setup() {
+    const setupLineStyle = () => {
+      this.context.lineWidth = 10
+      this.context.lineCap = 'round'
+      this.context.strokeStyle = '#ACD3ED'
+    }
+
+    const setupSnapshots = () => {
+      if (snapshots.length > 0) {
+        snapshots.forEach((snapshot) => setSnapshot(this.$canvas, snapshot))
+      }
+    }
+
+    setupLineStyle()
+    setupSnapshots()
+
+    this.addEventListener('mousemove', this.draw)
+    this.addEventListener('touchmove', this.draw)
+  }
+
+  cleanup() {
+    const takeSnapshot = () => {
+      const snapshot = getSnapshot(this.$canvas)
+      snapshot && snapshots.push(snapshot)
+    }
+
+    const resetPencilPoints = () => {
+      this.points = []
+    }
+
+    takeSnapshot()
+    resetPencilPoints()
+    this.removeEventListener('mousemove', this.draw)
+    this.removeEventListener('touchmove', this.draw)
+  }
+
+  draw(e: MouseEvent | TouchEvent) {
+    const trackTouchPoint = () => {
+      const { x, y } = getIsomorphicTouchPoint(this.$canvas, e)
+      this.points.push({ x, y })
+    }
+
+    const paint = () => {
+      this.context.beginPath()
+
+      // we can insure coordinate is exist because paint is called after track touch point
+      const { x: startX, y: startY } = this.points[0]
+      this.context.moveTo(startX, startY)
+
+      // draw smooth line with quadratic Bézier curve
+      for (let idx = 1; idx < this.points.length - 1; idx += 1) {
+        const endpoint = getMiddlePoint(this.points[idx], this.points[idx + 1])
+        this.context.quadraticCurveTo(
+          this.points[idx].x,
+          this.points[idx].y,
+          endpoint.x,
+          endpoint.y
+        )
+      }
+
+      this.context.stroke()
+    }
+
+    trackTouchPoint()
+    paint()
+  }
+}
+
+function getIsomorphicTouchPoint(canvas: HTMLCanvasElement, e: MouseEvent | TouchEvent) {
+  const rect = canvas.getBoundingClientRect()
+  const scaleX = canvas.width / rect.width
+  const scaleY = canvas.height / rect.height
+
+  if (e instanceof TouchEvent) {
+    // only deal with one finger touch
+    const touch = e.touches[0]
+
+    return {
+      x: (touch.clientX - rect.left) * scaleX,
+      y: (touch.clientY - rect.top) * scaleY,
+    }
+  } else {
+    return {
+      x: (e.clientX - rect.left) * scaleX,
+      y: (e.clientY - rect.top) * scaleY,
+    }
+  }
+}
+
+function getMiddlePoint(p1: PencilPoint, p2: PencilPoint) {
+  return {
+    x: p1.x + (p2.x - p1.x) / 2,
+    y: p1.y + (p2.y - p1.y) / 2,
+  }
+}
+
+function fillBackgroundColor(canvas: HTMLCanvasElement, color: string) {
+  const context = canvas.getContext('2d')
+  if (!context) {
+    return
+  }
+
+  context.fillStyle = color
+  context.fillRect(0, 0, canvas.width, canvas.height)
+}
+
+function getSnapshot(canvas: HTMLCanvasElement) {
+  const context = canvas.getContext('2d')
+  if (!context) {
+    return
+  }
+
+  return context.getImageData(0, 0, canvas.width, canvas.height)
+}
+
+function setSnapshot(canvas: HTMLCanvasElement, snapshot: ImageData) {
+  const context = canvas.getContext('2d')
+  if (!context) {
+    return
+  }
+
+  context.putImageData(snapshot, 0, 0)
 }
