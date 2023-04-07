c6560d7a58c850cee064613b962cb15f63985ad0
owen-triple <37819666+sohnjunior@users.noreply.github.com>
2023-04-07 21:16:42 +0900

    ✨ Add action select option

--- client/src/components/molecules/control-container/control-container.ts
+++ client/src/components/molecules/control-container/control-container.ts
@@ -1,5 +1,7 @@
 import { CanvasContext } from '@/contexts'
+import type { Phase } from '@/contexts'
 
+const OPTIONS: Phase[] = ['cursor', 'draw', 'erase', 'emoji']
 const template = document.createElement('template')
 template.innerHTML = `
   <style>
@@ -7,43 +9,58 @@ template.innerHTML = `
       display: flex;
       gap: 14px;
     }
+
+    :host v-icon-button[data-selected="true"] {
+      border-radius: 3px;
+      background-color: rgba(151, 222, 255, 0.4);
+    }
   </style>
   <v-container>
-    <v-icon-button data-icon="cursor" icon="cursor" size="medium"></v-icon-button>
-    <v-icon-button data-icon="pen" icon="pen" size="medium"></v-icon-button>
-    <v-icon-button data-icon="eraser" icon="eraser" size="medium"></v-icon-button>
-    <v-icon-button data-icon="emoji" icon="emoji" size="medium"></v-icon-button>
+    ${OPTIONS.map(
+      (control) =>
+        `<v-icon-button data-selected="false" data-icon="${control}" icon="${control}" size="medium"></v-icon-button>`
+    ).join('')}
   </v-container>
 `
 
 export default class VControlContainer extends HTMLElement {
-  private $root!: ShadowRoot
+  #$root!: ShadowRoot
+  #$selectRef?: HTMLElement
 
   static tag = 'v-control-container'
 
+  get phase() {
+    return CanvasContext.state.phase
+  }
+
   constructor() {
     const initShadowRoot = () => {
-      this.$root = this.attachShadow({ mode: 'open' })
-      this.$root.appendChild(template.content.cloneNode(true))
+      this.#$root = this.attachShadow({ mode: 'open' })
+      this.#$root.appendChild(template.content.cloneNode(true))
+    }
+
+    const initSelectedOption = () => {
+      this.setControlOption('draw')
     }
 
     super()
     initShadowRoot()
+    initSelectedOption()
   }
 
   connectedCallback() {
     const initEvents = () => {
-      const $container = this.$root.querySelector('v-container')
+      const $container = this.#$root.querySelector('v-container')
 
       $container?.addEventListener('click', (e) => {
         const $target = e.target as HTMLElement
 
         switch ($target.dataset.icon) {
-          case 'pen':
-            CanvasContext.dispatch({ action: 'SET_PHASE', data: 'drawing' })
+          case 'draw':
+            CanvasContext.dispatch({ action: 'SET_PHASE', data: 'draw' })
             break
-          case 'eraser':
-            CanvasContext.dispatch({ action: 'SET_PHASE', data: 'erasing' })
+          case 'erase':
+            CanvasContext.dispatch({ action: 'SET_PHASE', data: 'erase' })
             break
           default:
             return
@@ -51,6 +68,27 @@ export default class VControlContainer extends HTMLElement {
       })
     }
 
+    const subscribeContext = () => {
+      CanvasContext.subscribe({
+        action: 'SET_PHASE',
+        effect: (context) => {
+          this.setControlOption(context.state.phase)
+        },
+      })
+    }
+
     initEvents()
+    subscribeContext()
+  }
+
+  setControlOption(type: Phase) {
+    const $target = this.#$root.querySelector(`v-icon-button[data-icon="${type}"]`) as HTMLElement
+
+    if (this.#$selectRef) {
+      this.#$selectRef.dataset.selected = 'false'
+    }
+
+    this.#$selectRef = $target
+    this.#$selectRef.dataset.selected = 'true'
   }
 }
