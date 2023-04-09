e9daf27d620d29e72732372859d239549bf158e3
owen-triple <37819666+sohnjunior@users.noreply.github.com>
2023-04-09 23:52:42 +0900

    💡 Update comment (#11)

--- client/src/components/atoms/canvas/canvas.utils.ts
+++ client/src/components/atoms/canvas/canvas.utils.ts
@@ -4,14 +4,19 @@ export function isTouchEvent(e: unknown): e is TouchEvent {
   return window.TouchEvent && e instanceof TouchEvent
 }
 
-export function getSyntheticTouchPoint(canvas: HTMLCanvasElement, e: MouseEvent | TouchEvent) {
+/**
+ * 캔버스 요소 기준으로 선택된 터치(혹은 클릭) 지점을 px 단위로 반환합니다.
+ * @param canvas 대상 캔버스 요소
+ * @param ev MouseEvent 혹은 TouchEvent
+ */
+export function getSyntheticTouchPoint(canvas: HTMLCanvasElement, ev: MouseEvent | TouchEvent) {
   const rect = canvas.getBoundingClientRect()
   const scaleX = canvas.width / rect.width
   const scaleY = canvas.height / rect.height
 
-  if (isTouchEvent(e)) {
+  if (isTouchEvent(ev)) {
     // only deal with one finger touch
-    const touch = e.touches[0]
+    const touch = ev.touches[0]
 
     return {
       x: (touch.clientX - rect.left) * scaleX,
@@ -19,8 +24,8 @@ export function getSyntheticTouchPoint(canvas: HTMLCanvasElement, e: MouseEvent 
     }
   } else {
     return {
-      x: (e.clientX - rect.left) * scaleX,
-      y: (e.clientY - rect.top) * scaleY,
+      x: (ev.clientX - rect.left) * scaleX,
+      y: (ev.clientY - rect.top) * scaleY,
     }
   }
 }
