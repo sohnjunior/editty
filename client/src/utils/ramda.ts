d2d4ddb7f5fc41cc9226322d35135c59e6ffb7fb
owen-triple <37819666+sohnjunior@users.noreply.github.com>
2023-04-20 02:46:18 +0900

    ✨ Add filterNullish utility function and test (#19)

--- client/src/utils/ramda.ts
+++ client/src/utils/ramda.ts
@@ -1,3 +1,9 @@
 export function lastOf<T>(list: T[]): T | undefined {
   return list.slice(-1)[0]
 }
+
+export function filterNullish<T>(array: T[]) {
+  return array.filter<Exclude<T, null | undefined>>((el): el is Exclude<T, null | undefined> =>
+    Boolean(el)
+  )
+}
