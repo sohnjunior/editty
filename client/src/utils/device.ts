74621cac4b66fb2ece4faa8d8baae75caadcf030
owen-triple <37819666+sohnjunior@users.noreply.github.com>
2023-04-23 01:23:02 +0900

    ♻️ Move touch-event guard to device util

--- client/src/utils/device.ts
+++ client/src/utils/device.ts
@@ -1,3 +1,7 @@
 export function setDeviceCursor(cursorType: string) {
   document.body.style.cursor = cursorType
 }
+
+export function isTouchEvent(e: unknown): e is TouchEvent {
+  return window.TouchEvent && e instanceof TouchEvent
+}
