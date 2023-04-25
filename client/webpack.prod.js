3fde8f6e4e201a4421e567c65a8b337f57fe1566
owen-triple <37819666+sohnjunior@users.noreply.github.com>
2023-04-25 21:27:59 +0900

    🔧 Add copy-plugin for public image asset in production build

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -1,7 +1,14 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'hidden-source-map',
+  plugins: [
+    ...common.plugins,
+    new CopyPlugin({
+      patterns: [{ from: 'public/assets/images', to: 'assets/images' }],
+    }),
+  ],
 })
