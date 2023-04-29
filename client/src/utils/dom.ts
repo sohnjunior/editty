2749060d18b1e13ac0b9a9631cdda885cd5ea4ca
owen-triple <37819666+sohnjunior@users.noreply.github.com>
2023-04-29 16:18:56 +0900

    ⚰️ Remove unnecessary color query function (#36)

--- client/src/utils/dom.ts
+++ client/src/utils/dom.ts
@@ -1,13 +1,3 @@
-export function getGlobalColorPalette({
-  canvas,
-  color,
-}: {
-  canvas: HTMLCanvasElement
-  color: `--color-${string}`
-}) {
-  return getComputedStyle(canvas).getPropertyValue(color)
-}
-
 export function setMouseCursor(cursorType: string) {
   document.body.style.cursor = cursorType
 }
@@ -16,6 +6,7 @@ export function isTouchEvent(e: unknown): e is TouchEvent {
   return window.TouchEvent && e instanceof TouchEvent
 }
 
+// TODO: test-util 로 옮기기
 export function getSlotNodes(target: Element | null, slotName?: string) {
   const selector = slotName ? `slot[name="${slotName}"]` : 'slot'
   const slotElement: HTMLSlotElement | null = target?.shadowRoot?.querySelector(selector) ?? null
@@ -23,6 +14,7 @@ export function getSlotNodes(target: Element | null, slotName?: string) {
   return slotElement?.assignedNodes() ?? []
 }
 
+// TODO: test-util 로 옮기기
 export function getSlotElements(target: Element | null, slotName?: string) {
   const selector = slotName ? `slot[name="${slotName}"]` : 'slot'
   const slotElement: HTMLSlotElement | null = target?.shadowRoot?.querySelector(selector) ?? null
