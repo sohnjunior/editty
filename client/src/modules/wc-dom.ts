00f3b777cfbb91900f6e97b039ad1cfdf27e6480
owen-triple <37819666+sohnjunior@users.noreply.github.com>
2023-04-29 15:42:11 +0900

    ✨ Add web-component testing module (#36)

---
+++ client/src/modules/wc-dom.ts
@@ -0,0 +1,22 @@
+/**
+ * web component 테스트를 위한 dom query 입니다.
+ */
+
+/** 스타일태그를 제외하고 해당 web component 의 template root element 를 반환합니다. */
+export function getTemplateRootElement<T>(element: HTMLElement) {
+  const children = element.shadowRoot?.children ?? []
+  const rootElement = [...children].find((element) => !(element instanceof HTMLStyleElement))
+
+  return rootElement as T
+}
+
+/** 대상 web component 의 initStyle 훅에서 설정된 스타일값을 가져옵니다.  */
+export async function getInitialStyle(element: HTMLElement) {
+  await waitRAF()
+
+  return element.style
+}
+
+function waitRAF() {
+  return new Promise((resolve) => requestAnimationFrame(resolve))
+}
