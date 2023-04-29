b6db93fb1d55496ab1f11a8fb30143573f3302cc
owen-triple <37819666+sohnjunior@users.noreply.github.com>
2023-04-29 16:13:00 +0900

    ♻️ Use getInitialStyle util function (#36)

--- client/src/components/atoms/color-tile/color-tile.spec.ts
+++ client/src/components/atoms/color-tile/color-tile.spec.ts
@@ -17,14 +17,13 @@ describe('color-tile', () => {
 
     const colorTile = screen.getByTestId('color-tile')
     const rootElement = getTemplateRootElement<HTMLDivElement>(colorTile)
-
     const style = await getInitialStyle(rootElement)
 
     expect(style.width).toBe('10px')
     expect(style.height).toBe('10px')
   })
 
-  it('should accept color attribute', () => {
+  it('should accept color attribute', async () => {
     const color = {
       name: 'anakiwa',
       rgb: 'rgb(142, 202, 230)',
@@ -36,19 +35,21 @@ describe('color-tile', () => {
 
     const colorTile = screen.getByTestId('color-tile') as ColorTile
     const rootElement = getTemplateRootElement<HTMLDivElement>(colorTile)
+    const style = await getInitialStyle(rootElement)
 
-    expect(rootElement.style.backgroundColor).toBe(color.rgb)
+    expect(style.backgroundColor).toBe(color.rgb)
   })
 
-  it('should accept size attribute', () => {
+  it('should accept size attribute', async () => {
     document.body.innerHTML = `
       <v-color-tile data-testid="color-tile" color="anakiwa" size="20px"></v-color-tile>
     `
 
     const colorTile = screen.getByTestId('color-tile')
     const rootElement = getTemplateRootElement<HTMLDivElement>(colorTile)
+    const style = await getInitialStyle(rootElement)
 
-    expect(rootElement.style.width).toBe('20px')
-    expect(rootElement.style.height).toBe('20px')
+    expect(style.width).toBe('20px')
+    expect(style.height).toBe('20px')
   })
 })
