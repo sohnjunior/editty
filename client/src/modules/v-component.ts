f9ea93192a8ee3fa5019c1672b9d0994340c2424
owen-triple <37819666+sohnjunior@users.noreply.github.com>
2023-05-01 01:52:57 +0900

    ✨ Add initRootElement hook when v-component construct (#38)

--- client/src/modules/v-component.ts
+++ client/src/modules/v-component.ts
@@ -1,15 +1,31 @@
-export abstract class VComponent extends HTMLElement {
+export abstract class VComponent<R = HTMLElement> extends HTMLElement {
   static tag: string
   protected $shadow!: ShadowRoot
+  protected $root!: R
 
   constructor(template: HTMLTemplateElement) {
     const initShadowRoot = () => {
       this.$shadow = this.attachShadow({ mode: 'open' })
       this.$shadow.appendChild(template.content.cloneNode(true))
     }
+    const initRootElement = () => {
+      const children = this.$shadow.children
+      const elements = [...children].filter((element) => !(element instanceof HTMLStyleElement))
+
+      if (elements.length > 1) {
+        throw new Error('🚨 v-component must contain one root element')
+      }
+
+      if (elements.length === 0) {
+        throw new Error('🚨 v-component initialization fail')
+      }
+
+      this.$root = elements[0] as R
+    }
 
     super()
     initShadowRoot()
+    initRootElement()
   }
 
   // protected connectedCallback() {
