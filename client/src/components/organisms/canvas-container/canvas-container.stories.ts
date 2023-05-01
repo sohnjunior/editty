3de4032db6d3f1aae626725ec172681df2ee0098
owen-triple <37819666+sohnjunior@users.noreply.github.com>
2023-05-01 20:45:39 +0900

    ♻️ Squeeze unnecessary story (#38)

--- client/src/components/organisms/canvas-container/canvas-container.stories.ts
+++ client/src/components/organisms/canvas-container/canvas-container.stories.ts
@@ -1,6 +1,12 @@
 import { html } from 'lit-html'
+import CanvasBackgroundLayer from '@molecules/canvas-layer/background-layer'
+import CanvasDrawingLayer from '@molecules/canvas-layer/drawing-layer'
+import CanvasImageLayer from '@molecules/canvas-layer/image-layer'
 import CanvasContainer from './canvas-container'
 
+customElements.define(CanvasBackgroundLayer.tag, CanvasBackgroundLayer)
+customElements.define(CanvasDrawingLayer.tag, CanvasDrawingLayer)
+customElements.define(CanvasImageLayer.tag, CanvasImageLayer)
 customElements.define(CanvasContainer.tag, CanvasContainer)
 
 export default {
@@ -8,4 +14,8 @@ export default {
 }
 
 export const Basic = () =>
-  html`<div style="width: 500px; height: 700px"><v-canvas-container></v-canvas-container></div>`
+  html`
+    <div style="width: 500px; height: 700px">
+      <v-canvas-container></v-canvas-container>
+    </div>
+  `
