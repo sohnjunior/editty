9c4a818300e0945a3a0f792103604b99b2559aa9
owen-triple <37819666+sohnjunior@users.noreply.github.com>
2023-05-05 14:46:14 +0900

    🧪 Add icon unit test (#37)

---
+++ client/src/components/atoms/icon/icon.spec.ts
@@ -0,0 +1,27 @@
+import Icon from './icon'
+import { screen } from '@testing-library/dom'
+import { getTemplateRootElement, waitWCStyleInit } from '@/modules/wc-dom'
+
+describe('icon', () => {
+  beforeAll(() => {
+    customElements.define(Icon.tag, Icon)
+  })
+  afterEach(() => {
+    document.body.innerHTML = ''
+  })
+
+  it('should render icon with medium size', async () => {
+    document.body.innerHTML = `
+      <v-icon data-testid="icon" icon="emoji" size="medium"></v-icon>
+    `
+
+    await waitWCStyleInit()
+
+    const iconElement = screen.getByTestId('icon')
+    const rootElement = getTemplateRootElement<HTMLDivElement>(iconElement)
+
+    expect(iconElement.style.backgroundImage).toBeDefined()
+    expect(rootElement.style.width).toBe('15px')
+    expect(rootElement.style.height).toBe('15px')
+  })
+})
