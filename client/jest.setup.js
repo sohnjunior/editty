eb3f5d727cb2694eb453e592df7dd35da5d9c702
owen-triple <37819666+sohnjunior@users.noreply.github.com>
2023-05-06 16:58:51 +0900

    💡 Add friendly comment (#8)

--- client/jest.setup.js
+++ client/jest.setup.js
@@ -2,5 +2,6 @@ require('@testing-library/jest-dom')
 const { defineCustomElements } = require('./src/registry')
 
 beforeAll(() => {
+  // ✅ setup web-component before invoke test suites
   defineCustomElements()
 })
