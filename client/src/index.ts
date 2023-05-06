e170a98ccd8450f90e5741e307c02aff5185aeed
owen-triple <37819666+sohnjunior@users.noreply.github.com>
2023-05-06 17:12:57 +0900

    ♻️ Reorder statement for more readable code

--- client/src/index.ts
+++ client/src/index.ts
@@ -3,16 +3,16 @@ import { defineCustomElements } from './registry'
 import './reset.css'
 import './global.css'
 
-function mountApp() {
-  const $app = document.getElementById('app')
-  if ($app) {
-    $app.appendChild(document.createElement('v-app'))
-  }
-}
+initialize()
 
 function initialize() {
   defineCustomElements()
   mountApp()
 }
 
-initialize()
+function mountApp() {
+  const $app = document.getElementById('app')
+  if ($app) {
+    $app.appendChild(document.createElement('v-app'))
+  }
+}
