819727f476c4c4d560fe421677c847b4a60e3a48
owen-triple <37819666+sohnjunior@users.noreply.github.com>
2023-05-06 16:48:04 +0900

    ✨ Add path alias mapper to jest config (#8)

--- client/jest.config.js
+++ client/jest.config.js
@@ -5,5 +5,9 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
+    '^@atoms/(.*)$': '<rootDir>/src/components/atoms/$1',
+    '^@molecules/(.*)$': '<rootDir>/src/components/molecules/$1',
+    '^@organisms/(.*)$': '<rootDir>/src/components/organisms/$1',
+    '^@templates/(.*)$': '<rootDir>/src/components/templates/$1',
   },
 }
