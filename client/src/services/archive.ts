a8dcf61424ea84cc23e6a226e7d5b35af7825bb1
owen-triple <37819666+sohnjunior@users.noreply.github.com>
2023-05-14 18:44:45 +0900

    ✨ Implement archive service (#51)

---
+++ client/src/services/archive.ts
@@ -0,0 +1,22 @@
+import IndexedDB from '@/modules/storage/idb'
+import type { ImageObject } from '@molecules/canvas-layer/types'
+import type { UUID } from '@/utils/crypto'
+
+export interface Archive {
+  id: UUID
+  title: string
+  snapshot?: ImageData
+  images: Omit<ImageObject, 'ref'>[] // NOTE: HTMLImageElement 는 웹 스토리지에 저장할 수 없기 때문에 제외
+}
+
+export async function getArchive(id: string) {
+  return await IndexedDB.getInstance().getItem<Archive>(id)
+}
+
+export function addOrUpdateArchive(data: Archive) {
+  IndexedDB.getInstance().addOrUpdateItem(data)
+}
+
+export function deleteArchive(id: string) {
+  IndexedDB.getInstance().deleteItem(id)
+}
