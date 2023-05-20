17a739439a53383418a3b08c2b0f6299a90f1aa8
owen-triple <37819666+sohnjunior@users.noreply.github.com>
2023-05-20 13:10:12 +0900

    🧪 Add archive-menu unit test (#51)

---
+++ client/src/components/molecules/archive-menu/archive-menu.spec.ts
@@ -0,0 +1,32 @@
+import { screen } from '@testing-library/dom'
+import { renderToHtml, getTemplateRootElement, getInitialStyle } from '@/modules/wc-test-utils'
+
+import VArchiveMenu from './archive-menu'
+
+describe('archive-menu', () => {
+  it('should show menu with open="true"', async () => {
+    await renderToHtml(`
+      <v-archive-menu data-testid="archive-menu" open="true"></v-color-menu>
+    `)
+
+    const $menu = screen.getByTestId('archive-menu')
+    expect($menu).toBeVisible()
+  })
+
+  it('should lazy load archive property', async () => {
+    await renderToHtml(`
+      <v-archive-menu data-testid="archive-menu" open="true"></v-color-menu>
+    `)
+
+    const $menu = screen.getByTestId<VArchiveMenu>('archive-menu')
+    const $root = getTemplateRootElement<HTMLElement>($menu)
+
+    $menu.archives = [{ id: '1', title: '스냅샷1' }]
+
+    const $canvasPreview = $root.querySelector<HTMLDivElement>(
+      '.preview-container > v-canvas-preview'
+    )
+
+    expect($canvasPreview).toBeInTheDocument()
+  })
+})
