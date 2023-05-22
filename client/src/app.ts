bd6647a8201f16b76f41e672447eab7dd98cb62b
owen-triple <37819666+sohnjunior@users.noreply.github.com>
2023-05-23 00:03:23 +0900

    ✨ Add canvas snapshot save feedback toast (#51)

--- client/src/app.ts
+++ client/src/app.ts
@@ -1,5 +1,7 @@
 import { VComponent } from '@/modules/v-component'
 import { Z_INDEX } from '@/utils/constant'
+import { EventBus, EVENT_KEY } from '@/event-bus'
+import VToast from '@atoms/toast/toast'
 
 const template = document.createElement('template')
 template.innerHTML = `
@@ -17,6 +19,13 @@ template.innerHTML = `
       bottom: 40px;
       z-index: ${Z_INDEX.ACTION_LAYER};
     }
+
+    :host v-toast {
+      position: fixed;
+      top: 20px;
+      left: 20px;
+      z-index: ${Z_INDEX.TOAST_LAYER};
+    }
   </style>
 
   <v-mobile-layout>
@@ -24,6 +33,15 @@ template.innerHTML = `
       <v-canvas-container></v-canvas-container>
       <v-canvas-toolbox></v-canvas-toolbox>
       <v-history-toolbox></v-history-toolbox>
+
+      <v-toast variant="success" open="false" autoclose="true">
+        <span slot="title">성공</span>
+        <span slot="description">저장에 성공했습니다.</span>
+      </v-toast>
+      <v-toast variant="fail" open="false" autoclose="true">
+        <span slot="title">실패</span>
+        <span slot="description">저장에 실패했습니다.</span>
+      </v-toast>
     </main>
   </v-mobile-layout>
 `
@@ -31,7 +49,38 @@ template.innerHTML = `
 export default class App extends VComponent {
   static tag = 'v-app'
 
+  private $saveSuccessToast!: VToast
+  private $saveFailToast!: VToast
+
   constructor() {
     super(template)
+  }
+
+  protected afterCreated() {
+    this.initToastElement()
+  }
+
+  private initToastElement() {
+    const $successToast = this.$root.querySelector<VToast>('v-toast[variant="success"]')
+    const $failToast = this.$root.querySelector<VToast>('v-toast[variant="fail"]')
+    if (!$successToast || !$failToast) {
+      throw new Error('initialize fail')
+    }
+
+    this.$saveSuccessToast = $successToast
+    this.$saveFailToast = $failToast
+  }
+
+  subscribeEventBus() {
+    EventBus.getInstance().on(EVENT_KEY.SAVE_SUCCESS, this.onSaveSuccess.bind(this))
+    EventBus.getInstance().on(EVENT_KEY.SAVE_FAIL, this.onSaveFail.bind(this))
+  }
+
+  private onSaveSuccess() {
+    this.$saveSuccessToast.open = true
+  }
+
+  private onSaveFail() {
+    this.$saveFailToast.open = true
   }
 }
