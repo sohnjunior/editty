b831e2aee9384806bba1cb3e507a543df53eb61d
owen-triple <37819666+sohnjunior@users.noreply.github.com>
2023-05-31 15:09:34 +0900

    🧪 Add test suite for cartesian coordinate system api (#63)

--- client/src/modules/canvas-utils/engine.spec.ts
+++ client/src/modules/canvas-utils/engine.spec.ts
@@ -1,22 +1,14 @@
-import { isPointInsideRect, get2dMiddlePoint, get2dDistance, resizeRect } from './engine'
+import {
+  isPointInsideRect,
+  get2dMiddlePoint,
+  get2dDistance,
+  getRotatedCartesianRectCoordinate,
+  getCartesianCoordinate,
+  getCenterOfCartesianRect,
+  resizeRect,
+} from './engine'
 
-describe('geometry tool', () => {
-  it('get2dMiddlePoint', () => {
-    const pointA = { x: 0, y: 0 }
-    const pointB = { x: 2, y: 0 }
-
-    expect(get2dMiddlePoint(pointA, pointB)).toStrictEqual({ x: 1, y: 0 })
-  })
-
-  it('get2dDistance', () => {
-    const pointA = { x: 0, y: 0 }
-    const pointB = { x: 2, y: 0 }
-
-    expect(get2dDistance(pointA, pointB)).toBe(2)
-  })
-})
-
-describe('2d graphic tool', () => {
+describe('Canvas graphic tool', () => {
   describe('isPointInsideRect', () => {
     it('should detect point inside rect', () => {
       const example = {
@@ -76,6 +68,171 @@ describe('2d graphic tool', () => {
       } as const
 
       expect(resizeRect(example)).toStrictEqual({ sx: 100, sy: 200, width: 500, height: 500 })
+    })
+  })
+})
+
+describe('Cartesian Coordinate System', () => {
+  it('get2dMiddlePoint', () => {
+    const pointA = { x: 0, y: 0 }
+    const pointB = { x: 2, y: 0 }
+
+    expect(get2dMiddlePoint(pointA, pointB)).toStrictEqual({ x: 1, y: 0 })
+  })
+
+  it('get2dDistance', () => {
+    const pointA = { x: 0, y: 0 }
+    const pointB = { x: 2, y: 0 }
+
+    expect(get2dDistance(pointA, pointB)).toBe(2)
+  })
+
+  describe('getRotatedCartesianRectCoordinate', () => {
+    it('should get corner coordinates with rotate-angle: 0', () => {
+      const example = {
+        vertices: {
+          nw: { x: 50, y: 50 },
+          ne: { x: 150, y: 50 },
+          sw: { x: 50, y: 0 },
+          se: { x: 150, y: 0 },
+        },
+        degree: 0,
+      }
+
+      const result = getRotatedCartesianRectCoordinate(example)
+
+      expect(result).toStrictEqual({
+        nw: { x: 50, y: 50 },
+        ne: { x: 150, y: 50 },
+        sw: { x: 50, y: 0 },
+        se: { x: 150, y: 0 },
+      })
+    })
+
+    it('should get corner coordinates with rotate-angle: 30', () => {
+      const example = {
+        vertices: {
+          nw: { x: 50, y: 50 },
+          ne: { x: 150, y: 50 },
+          sw: { x: 50, y: 0 },
+          se: { x: 150, y: 0 },
+        },
+        degree: 30,
+      }
+
+      const result = getRotatedCartesianRectCoordinate(example)
+
+      expect(result).toStrictEqual({
+        nw: { x: 44, y: 22 },
+        ne: { x: 131, y: 72 },
+        sw: { x: 69, y: -22 },
+        se: { x: 156, y: 28 },
+      })
+    })
+
+    it('should get corner coordinates with rotate-angle: 210', () => {
+      const example = {
+        vertices: {
+          nw: { x: 50, y: 50 },
+          ne: { x: 150, y: 50 },
+          sw: { x: 50, y: 0 },
+          se: { x: 150, y: 0 },
+        },
+        degree: 210,
+      }
+
+      const result = getRotatedCartesianRectCoordinate(example)
+
+      expect(result).toStrictEqual({
+        nw: { x: 156, y: 28 },
+        ne: { x: 69, y: -22 },
+        sw: { x: 131, y: 72 },
+        se: { x: 44, y: 22 },
+      })
+    })
+
+    it('should get corner coordinates with rotate-angle: 360', () => {
+      const example = {
+        vertices: {
+          nw: { x: 50, y: 50 },
+          ne: { x: 150, y: 50 },
+          sw: { x: 50, y: 0 },
+          se: { x: 150, y: 0 },
+        },
+        degree: 360,
+      }
+
+      const result = getRotatedCartesianRectCoordinate(example)
+
+      expect(result).toStrictEqual({
+        nw: { x: 50, y: 50 },
+        ne: { x: 150, y: 50 },
+        sw: { x: 50, y: 0 },
+        se: { x: 150, y: 0 },
+      })
+    })
+
+    it('should get corner coordinates with rotate-angle: 720', () => {
+      const example = {
+        vertices: {
+          nw: { x: 50, y: 50 },
+          ne: { x: 150, y: 50 },
+          sw: { x: 50, y: 0 },
+          se: { x: 150, y: 0 },
+        },
+        degree: 720,
+      }
+
+      const result = getRotatedCartesianRectCoordinate(example)
+
+      expect(result).toStrictEqual({
+        nw: { x: 50, y: 50 },
+        ne: { x: 150, y: 50 },
+        sw: { x: 50, y: 0 },
+        se: { x: 150, y: 0 },
+      })
+    })
+  })
+
+  describe('getCenterOfCartesianRect', () => {
+    it('should get center coordinate of rectangle', () => {
+      const example = {
+        nw: { x: 1, y: 3 },
+        ne: { x: 3, y: 3 },
+        sw: { x: 1, y: 1 },
+        se: { x: 3, y: 1 },
+      }
+
+      const result = getCenterOfCartesianRect(example)
+
+      expect(result).toStrictEqual({
+        x: 2,
+        y: 2,
+      })
+    })
+  })
+
+  describe('getCartesianCoordinate', () => {
+    it('should get coordinate with rotated clockwise 90', () => {
+      const example = { x: 1, y: 1 }
+
+      const result = getCartesianCoordinate({ point: example, degree: -90 })
+
+      expect(result).toStrictEqual({
+        x: 1,
+        y: -1,
+      })
+    })
+
+    it('should get coordinate with rotated counter-clockwise 90', () => {
+      const example = { x: 1, y: 1 }
+
+      const result = getCartesianCoordinate({ point: example, degree: 90 })
+
+      expect(result).toStrictEqual({
+        x: -1,
+        y: 1,
+      })
     })
   })
 })
