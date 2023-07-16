9b59ac86c3b0a4eb12d615de4792b14c0f9e8a39
owen-triple <37819666+sohnjunior@users.noreply.github.com>
2023-07-16 15:31:01 +0900

    ✨ Add value control (#91)

--- client/src/components/atoms/text-input/text-input.stories.ts
+++ client/src/components/atoms/text-input/text-input.stories.ts
@@ -1,17 +1,28 @@
 import { html } from 'lit-html'
+import VTextInput from './text-input'
 
 export default {
   title: 'Elements / Text Input',
 }
 
 interface BasicProps {
+  value: string
   placeholder: string
   onChange: (e: CustomEvent) => void
 }
 
-export const Basic = ({ placeholder, onChange }: BasicProps) =>
-  html`<v-text-input @change="${onChange}" placeholder="${placeholder}"></v-text-input>`
+export const Basic = ({ value, placeholder, onChange }: BasicProps) => {
+  setTimeout(() => {
+    const $textInput = document.querySelector<VTextInput>('v-text-input')
+    if ($textInput) {
+      $textInput.value = value
+    }
+  }, 0)
+
+  return html` <v-text-input @change="${onChange}" placeholder="${placeholder}"> </v-text-input> `
+}
 Basic.args = {
+  value: 'text',
   placeholder: 'input example',
   onChange: (e: CustomEvent) => console.log(e.detail.value),
 }
